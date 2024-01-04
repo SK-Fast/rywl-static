ab96fda906eda2b1bad591589fc40052b6b34fe2
devy <60780448+SK-Fast@users.noreply.github.com>
2024-01-04 21:59:56 +0700

    pdf changes

--- libs/timetables.js
+++ libs/timetables.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 const { JSDOM } = require("jsdom")
 
-const url = "https://rayongwit.ac.th/%e0%b8%95%e0%b8%b2%e0%b8%a3%e0%b8%b2%e0%b8%87%e0%b9%80%e0%b8%a3%e0%b8%b5%e0%b8%a2%e0%b8%99/"
+const url = "https://rayongwit.ac.th/%e0%b8%95%e0%b8%b2%e0%b8%a3%e0%b8%b2%e0%b8%87%e0%b9%80%e0%b8%a3%e0%b8%b5%e0%b8%a2%e0%b8%99-2/"
 
 async function getTimetables() {
     const res = await axios.get(url)
